3cfc99a33fa66070eb5b9982c7a0e81808b9eb2c
Daniel D. Burrows <danielburrows12@gmail.com>
2025-04-09 02:40:52 -0400

    Final commit for Gradescope: hello_gradescope.py, output.txt, system_test.py

--- week-1/hello_gradescope.py
+++ week-1/hello_gradescope.py
@@ -1,10 +1,16 @@
-# Name:
-# OSU Email:
+# Name: Daniel D. Burrows
+# OSU Email: burrdani@oregonstate.edu
 # Course: CS261 - Data Structures
-# Assignment:
-# Due Date:
-# Description:
+# Assignment: 0 Environment Set-up
+# Due Date: 04/09/2025
+# Description: The purpose of this introductory assignment is to get to know more about yourself and your programming
+# experience so far and check your system to make sure you have all the needed tools to complete the assignments in this course.
+# Additionally, you will have practice in simple debugging.
+# This assignment is weighted much lower than the typical assignment weight.
 
 
 def gradescope_test():
     return 42
+
+if __name__ == '__main__':
+    print("Hello World!")
